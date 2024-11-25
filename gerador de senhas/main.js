a370213b801b7979ae8d032acecf50f2002febae
romulobueno <160503797+romulobueno@users.noreply.github.com>
2024-11-25 11:47:56 -0300

    Delete gerador de senhas directory

--- gerador de senhas/main.js
+++
@@ -1,6 +0,0 @@
-const numeroSenha=document.querySelector('.parametro-senha-texto');
-let tamanhoSenha=10;
-numeroSenha.textContent=tamanhoSenha;
-const botoes=document.querySelector('.parametro-senha-botao');
-
-botoes[0].onclick=diminuirTamanho;
